620ad101eb51f838ce46376df9cc9025af66d956
mauricio.rodrigues <mauricio.rodrigues@dcx.ufpb.br>
2025-11-20 15:14:01 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -33,7 +33,10 @@ document.querySelector('button[type="submit"]')
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
     }
-    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) || // mudança para quebra de teste de proposito aqui
+      emailField.value.includes('@gmail.com')
+    
+    ) {
       return showAndHideErrorMessage()
     }
     firstNameField.value = ''
